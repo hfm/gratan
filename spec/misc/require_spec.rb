1d503395f01b81300696ba6068187c02890857c0
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-05-26 12:48:52 +0900

    [wip] misc spec

--- spec/misc/require_spec.rb
+++ spec/misc/require_spec.rb
@@ -30,8 +30,12 @@ require '#{f.path}'
         }
       end
 
+      # expect(show_grants).to match_array [
+      #   "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+      #   "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      # ]
       expect(show_grants).to match_array [
-        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost'",
         "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
       ]
     end
@@ -68,8 +72,12 @@ require '#{f.path.sub(/\.rb\z/, '')}'
         }
       end
 
+      # expect(show_grants).to match_array [
+      #   "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+      #   "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
+      # ]
       expect(show_grants).to match_array [
-        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost' IDENTIFIED BY PASSWORD '*F2F68D0BB27A773C1D944270E5FAFED515A3FA40'",
+        "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'scott'@'localhost'",
         "GRANT SELECT, INSERT, UPDATE, DELETE ON `test`.* TO 'scott'@'localhost'",
       ]
     end
